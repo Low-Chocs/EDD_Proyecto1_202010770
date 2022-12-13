a4cff35ab765e99ef6662a7460e90730009948ce
Chocs <Luisitom@gmail.com>
2022-12-13 00:05:40 -0600

    admin frontend 25%

--- main.js
+++ main.js
@@ -19,13 +19,127 @@ function log_to_new_user_div(){
     div_user.style.display = "block";    
 }
 
+function log_to_admin_div(){
+    var div_login = document.getElementById('login_div');
+    var div_admin = document.getElementById('admin_div');
+    var div_user = document.getElementById('user_admin_div');
+    div_login.style.display = "none";    
+    div_admin.style.display = "block";
+    div_user.style.display = "block";
+}
+
 function new_user_div_to_log(){
     var div_login = document.getElementById('login_div');
     var div_user = document.getElementById('create_user_div');
     div_login.style.display = "block";
     div_user.style.display = "none";    
 }
+
+function admin_div_to_log(){
+    var div_login = document.getElementById('login_div');
+    var div_admin = document.getElementById('admin_div');
+    var div_user = document.getElementById('user_admin_div');
+    var div_name = document.getElementById('name_admin_div');
+    var div_artist = document.getElementById('artist_admin_div');
+    var div_scheduled_music = document.getElementById('sheduled_music_admin_div');
+    var div_podcast = document.getElementById('podcast_admin_div');
+    
+    div_login.style.display = "block";
+    div_admin.style.display = "none";  
+    div_name.style.display = "none";
+    div_user.style.display = "none";
+    div_artist.style.display = "none";
+    div_scheduled_music.style.display = "none";
+    div_podcast.style.display = "none";    
+}
 //END: HIDE AND SHOW DIVE ELEMENTS
+
+//BEGIN ADMIN NAVIGATION
+function user_nav(){
+    var div_user = document.getElementById('user_admin_div');
+    var div_name = document.getElementById('name_admin_div');
+    var div_artist = document.getElementById('artist_admin_div');
+    var div_scheduled_music = document.getElementById('sheduled_music_admin_div');
+    var div_podcast = document.getElementById('podcast_admin_div');
+
+    div_user.style.display = "block";
+    div_name.style.display = "none";
+    div_artist.style.display = "none";
+    div_scheduled_music.style.display = "none";
+    div_podcast.style.display = "none";   
+}
+
+//Name = Songs var
+function name_nav(){
+    var div_user = document.getElementById('user_admin_div');
+    var div_name = document.getElementById('name_admin_div');
+    var div_artist = document.getElementById('artist_admin_div');
+    var div_scheduled_music = document.getElementById('sheduled_music_admin_div');
+    var div_podcast = document.getElementById('podcast_admin_div');
+
+    div_name.style.display = "block";
+    div_user.style.display = "none";
+    div_artist.style.display = "none";
+    div_scheduled_music.style.display = "none";
+    div_podcast.style.display = "none";   
+}
+
+function artist_nav(){
+    var div_user = document.getElementById('user_admin_div');
+    var div_name = document.getElementById('name_admin_div');
+    var div_artist = document.getElementById('artist_admin_div');
+    var div_scheduled_music = document.getElementById('sheduled_music_admin_div');
+    var div_podcast = document.getElementById('podcast_admin_div');
+
+    div_artist.style.display = "block";
+    div_user.style.display = "none";
+    div_name.style.display = "none";
+    div_scheduled_music.style.display = "none";
+    div_podcast.style.display = "none";   
+}
+
+function scheduled_music_nav(){
+    var div_user = document.getElementById('user_admin_div');
+    var div_name = document.getElementById('name_admin_div');
+    var div_artist = document.getElementById('artist_admin_div');
+    var div_scheduled_music = document.getElementById('sheduled_music_admin_div');
+    var div_podcast = document.getElementById('podcast_admin_div');
+
+    div_scheduled_music.style.display = "block";
+    div_name.style.display = "none";
+    div_artist.style.display = "none";
+    div_user.style.display = "none";
+    div_podcast.style.display = "none";   
+}
+
+function podcast_nav(){
+    var div_user = document.getElementById('user_admin_div');
+    var div_name = document.getElementById('name_admin_div');
+    var div_artist = document.getElementById('artist_admin_div');
+    var div_scheduled_music = document.getElementById('sheduled_music_admin_div');
+    var div_podcast = document.getElementById('podcast_admin_div');
+
+    div_podcast.style.display = "block";
+    div_name.style.display = "none";
+    div_artist.style.display = "none";
+    div_scheduled_music.style.display = "none";
+    div_user.style.display = "none";   
+}
+
+function user_nav(){
+    var div_user = document.getElementById('user_admin_div');
+    var div_name = document.getElementById('name_admin_div');
+    var div_artist = document.getElementById('artist_admin_div');
+    var div_scheduled_music = document.getElementById('sheduled_music_admin_div');
+    var div_podcast = document.getElementById('podcast_admin_div');
+
+    div_user.style.display = "block";
+    div_name.style.display = "none";
+    div_artist.style.display = "none";
+    div_scheduled_music.style.display = "none";
+    div_podcast.style.display = "none";   
+}
+//END ADMIN NAVIGATION
 
 //BEGIN: SIMPLE LINKED LIST
 class simpleNode{
@@ -80,7 +194,7 @@ class simpleLinkedList{
             aux = aux.next;
         }
         if(validation && aux.admin){
-            console.log("Admin")
+            log_to_admin_div()
         }else if(validation && !aux.admin){
             console.log("No admin but logged")
         }else{
@@ -89,7 +203,10 @@ class simpleLinkedList{
         }
     }
 }
+
 //END: SIMPLE LINKED LIST
+
+
 
 
 var linked_list = new simpleLinkedList();
@@ -97,7 +214,26 @@ var linked_list = new simpleLinkedList();
 function log_in(){
     linked_list.login()
 }
+
+function register_new_user(){
+    var new_user = document.getElementById('new_user_input').value;
+    var new_name = document.getElementById('new_name_input').value;
+    var new_pass = document.getElementById('new_pass_input').value;
+    var new_dpi = document.getElementById('new_dpi_input').value;
+    var new_phone = document.getElementById('new_phone_input').value;
+    console.log(new_user)
+
+    if(new_user != "" && new_name != "" && new_pass != "" && new_dpi != "" && new_phone != ""){
+        linked_list.insert(new_user, new_name, new_pass, new_dpi, new_phone)
+    }else{
+        alert("No dejes espacios vacíos!")
+    }
+
+
+}
+
+
 linked_list.insert("EDD", "Oscar Armi","123","2654568452521","+502 (123) 123-4567", true)
-linked_list.insert("EDD", "Oscar Armi","1234","2654568452521","+502 (123) 123-4567", false)
-linked_list.show()
+
+
 
