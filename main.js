10cd5775083ef04a26afae33cebb97a9eb3f32bd
Chocs <Luisitom@gmail.com>
2022-12-14 02:36:48 -0600

    Artist Graph WIP

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var log_user = document.getElementById('user_input');
 var log_password = document.getElementById('pass_input');
 
 
+
 function show_password(){
     cb = document.getElementById('check_pass').checked;
     if(cb){
@@ -39,14 +40,12 @@ function admin_div_to_log(){
     var div_login = document.getElementById('login_div');
     var div_admin = document.getElementById('admin_div');
     var div_user = document.getElementById('user_admin_div');
-    var div_name = document.getElementById('name_admin_div');
     var div_artist = document.getElementById('artist_admin_div');
     var div_scheduled_music = document.getElementById('sheduled_music_admin_div');
     var div_podcast = document.getElementById('podcast_admin_div');
-    
+
     div_login.style.display = "block";
     div_admin.style.display = "none";  
-    div_name.style.display = "none";
     div_user.style.display = "none";
     div_artist.style.display = "none";
     div_scheduled_music.style.display = "none";
@@ -57,56 +56,37 @@ function admin_div_to_log(){
 //BEGIN ADMIN NAVIGATION
 function user_nav(){
     var div_user = document.getElementById('user_admin_div');
-    var div_name = document.getElementById('name_admin_div');
     var div_artist = document.getElementById('artist_admin_div');
     var div_scheduled_music = document.getElementById('sheduled_music_admin_div');
     var div_podcast = document.getElementById('podcast_admin_div');
 
     div_user.style.display = "block";
-    div_name.style.display = "none";
     div_artist.style.display = "none";
     div_scheduled_music.style.display = "none";
     div_podcast.style.display = "none";   
 }
 
-//Name = Songs var
-function name_nav(){
-    var div_user = document.getElementById('user_admin_div');
-    var div_name = document.getElementById('name_admin_div');
-    var div_artist = document.getElementById('artist_admin_div');
-    var div_scheduled_music = document.getElementById('sheduled_music_admin_div');
-    var div_podcast = document.getElementById('podcast_admin_div');
 
-    div_name.style.display = "block";
-    div_user.style.display = "none";
-    div_artist.style.display = "none";
-    div_scheduled_music.style.display = "none";
-    div_podcast.style.display = "none";   
-}
 
 function artist_nav(){
     var div_user = document.getElementById('user_admin_div');
-    var div_name = document.getElementById('name_admin_div');
     var div_artist = document.getElementById('artist_admin_div');
     var div_scheduled_music = document.getElementById('sheduled_music_admin_div');
     var div_podcast = document.getElementById('podcast_admin_div');
 
     div_artist.style.display = "block";
     div_user.style.display = "none";
-    div_name.style.display = "none";
     div_scheduled_music.style.display = "none";
     div_podcast.style.display = "none";   
 }
 
 function scheduled_music_nav(){
     var div_user = document.getElementById('user_admin_div');
-    var div_name = document.getElementById('name_admin_div');
     var div_artist = document.getElementById('artist_admin_div');
     var div_scheduled_music = document.getElementById('sheduled_music_admin_div');
     var div_podcast = document.getElementById('podcast_admin_div');
 
     div_scheduled_music.style.display = "block";
-    div_name.style.display = "none";
     div_artist.style.display = "none";
     div_user.style.display = "none";
     div_podcast.style.display = "none";   
@@ -114,13 +94,11 @@ function scheduled_music_nav(){
 
 function podcast_nav(){
     var div_user = document.getElementById('user_admin_div');
-    var div_name = document.getElementById('name_admin_div');
     var div_artist = document.getElementById('artist_admin_div');
     var div_scheduled_music = document.getElementById('sheduled_music_admin_div');
     var div_podcast = document.getElementById('podcast_admin_div');
 
     div_podcast.style.display = "block";
-    div_name.style.display = "none";
     div_artist.style.display = "none";
     div_scheduled_music.style.display = "none";
     div_user.style.display = "none";   
@@ -128,13 +106,11 @@ function podcast_nav(){
 
 function user_nav(){
     var div_user = document.getElementById('user_admin_div');
-    var div_name = document.getElementById('name_admin_div');
     var div_artist = document.getElementById('artist_admin_div');
     var div_scheduled_music = document.getElementById('sheduled_music_admin_div');
     var div_podcast = document.getElementById('podcast_admin_div');
 
     div_user.style.display = "block";
-    div_name.style.display = "none";
     div_artist.style.display = "none";
     div_scheduled_music.style.display = "none";
     div_podcast.style.display = "none";   
@@ -202,14 +178,186 @@ class simpleLinkedList{
             console.log("No response")
         }
     }
+
+    graph(){
+        var codigodot = "digraph G{\nlabel=\" Usuarios \";\nnode [shape=box];\n graph [rankdir = LR];";
+        var temporal = this.head
+        var conexiones ="";
+        var nodos ="";
+        var numnodo= 0;
+        while (temporal != null) {
+            nodos+=  "N" + numnodo + "[label=\"Usuario: " + temporal.username + " \n Nombre: " + temporal.name + " \n Contraseña: " + temporal.password + " \n DPI: " + temporal.dpi + "\nTel: " + temporal.phone + "\nAdimn: " + temporal.admin + "\"];\n"
+            if(temporal.next != null){
+                var auxnum = numnodo+1
+                conexiones += "N" + numnodo + " -> N" + auxnum + ";\n"
+            }
+            temporal = temporal.next
+            numnodo++;            
+        }
+        codigodot += "//agregando nodos\n"
+        codigodot += nodos+"\n"
+        codigodot += "//agregando conexiones o flechas\n"
+        codigodot += "{\n"+conexiones+"\n}\n}"
+        console.log(codigodot)
+        d3.select("#user_graph").graphviz()
+            .renderDot(codigodot)
+    }
 }
 
 //END: SIMPLE LINKED LIST
 
 
+//BEGIN: LIST OF LIST
+class song_Node{
+    constructor(_artist, _name, _duration, _gender){
+        this.artist = _artist;
+        this.name = _name;
+        this.duration = _duration;
+        this.gender = _gender; 
+        this.next = null;
+    }
+
+
+}
+
+class song_List{
+
+    constructor(){
+        this.head = null ;
+        this.bottom = null;
+        this.quantity = 0;
+
+    }
+
+    insert_Song(_artist, _name, _duaration, _gender){
+        var new_song = new song_Node(_artist, _name, _duaration, _gender)
+
+        if(this.quantity == 0){
+            this.head = new_song;
+            this.buttom = new_song;
+            this.quantity++;
+        }else{
+            this.buttom.next = new_song;
+            this.buttom = new_song;
+            this.quantity++;
+        }
+    }
+
+    show(){
+        var aux = this.head;
+        for(var i = 0; i< this.quantity; i++){
+            console.log(aux);
+            aux = aux.next;
+        }
+    }
+
+    songs_Saved(){
+        return this.quantity;
+    }
+}
+
+class artist_Node{
+    constructor(_name, _age, _country){
+        this.name = _name;
+        this.age = _age;
+        this.country = _country; 
+        this.song_list = new song_List();
+        this.next = null;
+    }
+}
+
+class artist_List{
+
+    constructor(){
+        this.head = null ;
+        this.bottom = null;
+        this.quantity = 0;
+
+    }
+
+    insert_Artist(_name, _age, _country){
+        var new_artist = new artist_Node(_name, _age, _country)
+
+        if(this.quantity == 0){
+            this.head = new_artist;
+            this.buttom = new_artist;
+            this.quantity++;
+        }else{
+            this.buttom.next = new_artist;
+            this.buttom = new_artist;
+            this.quantity++;
+        }
+    }
+
+    show(){
+        var aux = this.head;
+        for(var i = 0; i< this.quantity; i++){
+            console.log(aux);
+            aux = aux.next;
+        }
+    }
+
+    add_song(_artist, _name, _duaration, _gender){
+        var aux = this.head;
+        for(var i = 0; i< this.quantity; i++){
+            if(aux.name == _artist){
+                aux.song_list.insert_Song(_artist, _name, _duaration, _gender);
+                break;
+            }
+            aux = aux.next;
+        }
+    }
+
+    songs_Saved(){
+        var aux = this.head;
+        for(var i = 0; i< this.quantity; i++){
+            if(aux.name == _artist){
+                return aux.song_list
+                break;
+            }
+            aux = aux.next;
+        }
+    }
+
+    graph(){
+        var codigodot = "digraph G{\nlabel=\" Usuarios \";\nnode [shape=box];\n ";
+        var aux = this.head
+        var conexiones ="";
+        var nodos ="";
+        var numnodo= 0;
+        for(var i=0; i<this.quantity;i++){
+            codigodot += "U"+i+"[label = \""+aux.name+"\" width = 1.5 style = filled, fillcolor = bisque1, group = 1 ];\n";
+            aux= aux.next;
+        }
+        if(this.quantity > 1){
+            for(var i = 0; i < this.quantity - 1; i++){
+                codigodot += "U"+i+"->"+"U"+(i+1)+";";
+        }
+
+        //       var aux2 = this.head;
+        //var counter = 0;
+        //for(var i = 0; i < this.quantity; i++){
+            //if(aux2.name == _artist){
+              //  aux.song_list.quantity
+                
+            //}
+          //  aux = aux.next;
+        //}
+
+        }
+        codigodot+="}"
+        console.log(codigodot)
+        d3.select("#artist_graph").graphviz()
+            .renderDot(codigodot)
+    }
+}
+
+//END: LIST OF LIST
+
 
 
 var linked_list = new simpleLinkedList();
+var artist_list = new artist_List();
 
 function log_in(){
     linked_list.login()
@@ -217,14 +365,13 @@ function log_in(){
 
 function register_new_user(){
     var new_user = document.getElementById('new_user_input').value;
-    var new_name = document.getElementById('new_name_input').value;
     var new_pass = document.getElementById('new_pass_input').value;
     var new_dpi = document.getElementById('new_dpi_input').value;
     var new_phone = document.getElementById('new_phone_input').value;
     console.log(new_user)
 
     if(new_user != "" && new_name != "" && new_pass != "" && new_dpi != "" && new_phone != ""){
-        linked_list.insert(new_user, new_name, new_pass, new_dpi, new_phone)
+        linked_list.insert(new_user, new_name, new_pass, new_dpi, new_phone, false)
     }else{
         alert("No dejes espacios vacíos!")
     }
@@ -232,8 +379,113 @@ function register_new_user(){
 
 }
 
+function graphUser(){
+    linked_list.graph()
+}
 
+
+function loadUser(e) {
+    var file = document.getElementById("userJson").files[0];
+    if (!file) {
+      return;
+    }
+    let reader = new FileReader();
+    reader.onload = function (e) {
+      let content = e.target.result;
+  
+      const _data = JSON.parse(content);
+  
+      for (const i in _data) {
+        let data = _data[i];
+        linked_list.insert(data.username, data.name, data.password, data.dpi,  data.phone, data.admin)
+      }
+    };
+    reader.readAsText(file);
+}
+
+function loadArtist(e) {
+    var file = document.getElementById("artistJson").files[0];
+    if (!file) {
+      return;
+    }
+    let reader = new FileReader();
+    reader.onload = function (e) {
+      let content = e.target.result;
+  
+      const _data = JSON.parse(content);
+  
+      for (const i in _data) {
+        let data = _data[i];
+        artist_list.insert_Artist(data.name, data.age, data.country)
+      }
+    };
+    reader.readAsText(file);
+ 
+}
+
+
+function loadSongs(e) {
+    var file = document.getElementById("songsJson").files[0];
+    if (!file) {
+      return;
+    }
+    let reader = new FileReader();
+    reader.onload = function (e) {
+      let content = e.target.result;
+  
+      const _data = JSON.parse(content);
+  
+      for (const i in _data) {
+        let data = _data[i];
+        artist_list.add_song(data.artist, data.name, data.duration, data.gender)
+      }
+    };
+    reader.readAsText(file);
+}
+
+function loadScheduled(e) {
+    var file = document.getElementById("scheduledJson").files[0];
+    if (!file) {
+      return;
+    }
+    let reader = new FileReader();
+    reader.onload = function (e) {
+      let content = e.target.result;
+  
+      const _data = JSON.parse(content);
+  
+      for (const i in _data) {
+        let data = _data[i];
+        console.log(data.month, data.day, data.song, data.artist)
+      }
+    };
+    reader.readAsText(file);
+}
+
+function loadPodcast(e) {
+    var file = document.getElementById("podcastJson").files[0];
+    if (!file) {
+      return;
+    }
+    let reader = new FileReader();
+    reader.onload = function (e) {
+      let content = e.target.result;
+  
+      const _data = JSON.parse(content);
+  
+      for (const i in _data) {
+        let data = _data[i];
+        console.log(data.name, data.topic, data.duration, data.guests)
+      }
+    };
+    reader.readAsText(file);
+}
+function graph_Artist(){
+    artist_list.graph()
+}
 linked_list.insert("EDD", "Oscar Armi","123","2654568452521","+502 (123) 123-4567", true)
+
+
 
 
 
